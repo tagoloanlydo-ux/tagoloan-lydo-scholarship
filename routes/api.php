e427185b9d23451fee53293fad682591a9dbb16d
tagoloanlydo-ux <tagoloanlydo@gmail.com>
2025-10-30 18:29:37 +0800

    c

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\ApplicantController;
 use App\Http\Controllers\API\ApplicationController;
-use App\Http\Controllers\API\ApplicationPersonnelController;
 use App\Http\Controllers\API\ScholarController;
 use App\Http\Controllers\API\AdminController;
 use App\Http\Controllers\API\NotificationController;
@@ -15,25 +14,21 @@ use App\Http\Controllers\API\ReportController;
 use App\Http\Controllers\API\DisbursementController;
 use App\Http\Controllers\API\RenewalController;
 use App\Http\Controllers\API\AnnouncementController;
-use App\Http\Controllers\MayorStaffController;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+// Wrap all routes in staging prefix to match Flutter app expectations
+Route::prefix('staging')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 
-Route::apiResource('/applicants', ApplicantController::class);
-Route::apiResource('/applications', ApplicationController::class);
-Route::apiResource('/application-personnels', ApplicationPersonnelController::class);
-
-Route::apiResource('/scholars', ScholarController::class);
-Route::apiResource('/renewals', RenewalController::class);
-Route::apiResource('/disbursements', DisbursementController::class);
-Route::apiResource('/announcements', AnnouncementController::class);
-Route::apiResource('/reports', ReportController::class);
-Route::apiResource('/notifications', NotificationController::class);
-Route::apiResource('/admins', AdminController::class);
-
-// Mayor Staff API rgffffggsdsadggoutes
-Route::get('/mayor-staff/welcome', [MayorStaffController::class, 'welcomeApi']);
-Route::get('/mayor-staff/intake-sheet/{application_personnel_id}', [MayorStaffController::class, 'getIntakeSheet']);
+    Route::apiResource('/applicants', ApplicantController::class);
+    Route::apiResource('/applications', ApplicationController::class);
+    Route::apiResource('/scholars', ScholarController::class);
+    Route::apiResource('/renewals', RenewalController::class);
+    Route::apiResource('/disbursements', DisbursementController::class);
+    Route::apiResource('/announcements', AnnouncementController::class);
+    Route::apiResource('/reports', ReportController::class);
+    Route::apiResource('/notifications', NotificationController::class);
+    Route::apiResource('/admins', AdminController::class);
+});
 
 ?>
